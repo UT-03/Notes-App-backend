f1eb72c31cfcde236e80d7f9c6ea21fc40109d14
Utkarsh Tiwari <f20200458@pilani.bits-pilani.ac.in>
2022-05-26 21:49:54 +0530

    feat: Initial Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const HttpError = require('./util/HttpError');
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+});
+
+// Routes here
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route!', 404);
+    throw error;
+});
+
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3ogsy.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log("Connected to database!")
+        app.listen(PORT);
+    })
+    .then(() => {
+        console.log(`Server started at http://localhost:${PORT}`);
+    })
+    .catch(err => {
+        console.log(err);
+    });
